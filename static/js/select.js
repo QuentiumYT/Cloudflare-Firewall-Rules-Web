ad622ae2152ede6f75ec55b5e65143e46aa28e07
QuentiumYT <quentin.lienhardt@gmail.com>
2022-05-12 16:10:58 +0200

    Persistent domain storage select

--- static/js/select.js
+++ static/js/select.js
@@ -7,4 +7,17 @@ actionSelect.set("");
 let domainSelect = new SlimSelect({
     select: "#domain",
     allowDeselectOption: true,
+    closeOnSelect: false,
+    deselectLabel: "✘",
+    onChange: (domains) => {
+        domains = domains.map((domain) => {
+            return domain.value;
+        });
+        localStorage.setItem("domains", JSON.stringify(domains));
+    }
 });
+
+if (localStorage.getItem("domains")) {
+    const domains = JSON.parse(localStorage.getItem("domains"));
+    domainSelect.set(domains);
+}
